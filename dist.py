8e2b8aa289ecab9e8177c1fd6fbfb859f1ee299c
Tom Henderson <tomh@tomh.org>
2011-12-27 13:09:50 -0800

    remove tab character

--- dist.py
+++ dist.py
@@ -22,7 +22,7 @@ def tar_add_tree(tar, srcdir, tgtdir, exclude_dir_cb, exclude_file_cb):
         while True:
             for i, dirname in enumerate(dirnames):
                 if os.path.islink (os.path.join(dirpath,dirname)):
-		    # Add, but do not follow, symbolic links
+                    # Add, but do not follow, symbolic links
                     srcpath = os.path.join(dirpath, dirname)
                     tgtpath = os.path.join(tgtdir, reldirpath, dirname)
                     tar.add(srcpath, tgtpath)
